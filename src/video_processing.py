7e9ca8bcfdb040b2b6c35595c78e271de60b4c9f
Blackhurst Laurence <l.blackhurst19@imperial.ac.uk>
2023-04-24 18:03:30 +0100

    background extraction test

--- src/video_processing.py
+++ src/video_processing.py
@@ -1,15 +1,47 @@
 import cv2
 
-def remove_background(frame):
-    # Apply background subtraction to extract the foreground mask
+def remove_background(video_path):
+    cap = cv2.VideoCapture(video_path)
+
+    # Create background subtraction object
     fgbg = cv2.createBackgroundSubtractorMOG2()
-    fgmask = fgbg.apply(frame)
-    
-    # Apply morphological opening to remove noise and smooth the mask
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-    
-    # Invert the mask and apply it to the original frame to extract the foreground object
-    fg = cv2.bitwise_and(frame, frame, mask=fgmask)
-    bg = cv2.cvtColor(cv2.bitwise_not(fgmask), cv2.COLOR_GRAY2BGR)
-    return cv2.add(fg, bg)
+
+    # Define codec for output video
+    fourcc = cv2.VideoWriter_fourcc(*'H264')
+
+    # Set up output video file
+    out_path = r'C:\Users\lab\Documents\Projects\hd_videos\output.mp4'
+    print(cap.get(cv2.CAP_PROP_FPS), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    out = cv2.VideoWriter(out_path, fourcc, cap.get(cv2.CAP_PROP_FPS), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+
+        # Apply background subtraction
+        fgmask = fgbg.apply(frame)
+        cv2.imshow('fgmask', fgmask)
+        cv2.imshow('frame',frame )
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        # Write frame to output video
+        out.write(fgmask)
+        # out.write(frame)
+
+    # Release resources
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+    return out_path
+
+
+video_path = r'C:\Users\lab\Documents\Projects\hd_videos\short_test.mp4'
+
+new_vid = remove_background(video_path)
+# print(new_vid)
